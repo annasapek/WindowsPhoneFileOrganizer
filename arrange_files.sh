3fe007e15a9f67a50e19bcf47aa5ade4eadd20da
Anna <git@anna.sapek.com>
2015-03-31 20:46:17 -0700

    Allow user to specify destination directory

--- arrange_files.sh
+++ arrange_files.sh
@@ -1,29 +1,31 @@
+#
+# Windows Phone File Organizer
+#
 
 # Path to Windows Phone import folder
-ORIGIN="/path/to/From Windows Phone/Camera Roll/"
-ROOT=`pwd`
-mv "$ORIGIN"* .
+ORIGIN="/cygdrive/c/Users/username/Pictures/From Windows Phone/Camera Roll/"
 
-for file in `ls *.jpg *.mp4`
+# Destination directory
+ROOT="/cygdrive/e/destination/directory/"
+
+for file in `ls "$ORIGIN"`
 do
-	YEAR=${file:3:4};
-
-	MONTH=${file:7:2};
+	YEAR=${file:3:4}
+	MONTH=${file:7:2}
 	
 	# If the year directory does not exist, create it
-	if [[ ! -d ./$YEAR ]]
+	if [[ ! -d "$ROOT"$YEAR ]]
 	then
 		echo "Creating new directory $ROOT/$YEAR"
-		mkdir ./$YEAR
+		mkdir "$ROOT"$YEAR
 	fi
 	
 	# If the year/month directory does not exist, create it
-	if [[ ! -d ./$YEAR/$MONTH ]]
+	if [[ ! -d "$ROOT"$YEAR/$MONTH ]]
 	then
 		echo "Creating new directory $ROOT/$YEAR/$MONTH"
-		mkdir ./$YEAR/$MONTH
+		mkdir "$ROOT"$YEAR/$MONTH
 	fi
 	
-	mv $file ./$YEAR/$MONTH/
-	
+	mv "$ORIGIN"$file "$ROOT"$YEAR/$MONTH/
 done
